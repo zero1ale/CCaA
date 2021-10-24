4bb132be6ecc7d6a264f5f73127573afbf933efa
zero1ale <rootzero01@gmail.com>
2021-10-24 03:32:03 -0500

    version entregable website

--- app.py
+++ app.py
@@ -11,6 +11,14 @@ def home():
 def transition():
     return render_template('transition.html')
 
+@app.route('/user_dash.html')
+def dash():
+    return render_template('user_dash.html')
+
+@app.route('/comsult.html')
+def consult():
+    return render_template('comsult.html')
+
 # Make sure this we are executing this file
 if __name__ == '__main__':
     app.run(debug=True)
